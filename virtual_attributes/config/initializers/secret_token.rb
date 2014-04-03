2e9d5c0f4dec913fdc334b91297750759d278c6b
Apprentice <apprentice@dbc-chicago-002.local>
2014-04-03 18:24:05 -0500

    init and added rails skeleton

---
+++ virtual_attributes/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VirtualAttributes::Application.config.secret_key_base = 'cc07e53fd3617cabdb78be0a08178a8eb3945b51f0bc71463258e5709cf5c95dbc5518b6f343c9838665244eeb4dbb604a72655a4dd75a3df81972540803b130'
